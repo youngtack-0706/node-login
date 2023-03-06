afb47a176fb49f131e0054ca5003b1a2157d1ea9
youngtack-0706 <rladudxor590@gmail.com>
2023-03-07 02:46:14 +0900

    로그인 버튼 클릭시 값이 스크립트로 전달되게 변경

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,16 @@
-console.log("hello my script");
-console.log("bye bye");
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginBtn = document.querySelector("#loginBtn");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        pw: pw.value
+    }
+    
+    //제이슨 값을 출력하는 방법. 안하면 [object Object]라고 뜸
+    console.log("req: "+JSON.stringify(req));
+}
+

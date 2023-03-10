b7b6ae14b929f9435a16da4b2a98d4c673d3a305
youngtack-0706 <rladudxor590@gmail.com>
2023-03-10 09:13:02 +0900

    회원가입 정보 입력안하면 경고 글 뜨게함

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -40,19 +40,30 @@ function register(){
     // }, {});
 
 
-    // const registerInfo = ["name", "id", "pw", "confirmPw"];
+    const registerInfo = ["name", "id", "pw", "confirmPw"];
     
-    // registerInfo.reduce((newUsers, info, acc, curr) =>{
-    //     if(!req[info]){
-    //         curr.splice(1);  // eject early
-    //         // console.log("값 없음: ", info)
-    //         document.getElementById(info+"-check").hidden = false;
-    //         return;
-    //     }else{
-    //         // console.log("값 있음: ", info)
-    //         document.getElementById(info+"-check").hidden = true;
-    //     }
-    // }, {})
+    registerInfo.reduce((newUsers, info, acc, curr) =>{
+        if(!req[info]){
+            const regiInfo = JSON.parse(JSON.stringify(registerInfo))
+            var count = 0;
+            for(var i=acc; i<registerInfo.length; i++){
+                document.getElementById(registerInfo[i]+"-check").hidden = false;
+                curr.splice(acc);  // eject early
+                count = i+1;
+            }
+            if(count < regiInfo.length){
+                for(var i=count; i<regiInfo.length; i++){
+                    document.getElementById(regiInfo[i]+"-check").hidden = true;
+                }
+            }
+
+            console.log("값 없음: ", info)
+            return;
+        }else{
+            console.log("값 있음: ", info)
+            document.getElementById(info+"-check").hidden = true;
+        }
+    }, {})
 
     //제이슨 값을 출력하는 방법. 안하면 [object Object]라고 뜸
 
